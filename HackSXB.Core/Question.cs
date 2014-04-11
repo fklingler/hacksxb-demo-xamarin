d1adb8e00638b86e390a4ed2541f4956a14a17bd
François KLINGLER <francois@fklingler.com>
2014-04-11 16:07:55 +0200

    Add Question model with call to SO api

---
+++ HackSXB.Core/Question.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace HackSXB.Core
+{
+    public class Question
+    {
+        public string Title { get; set; }
+
+        public Question ()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+            
+        public delegate void FindXamarinQuestionsCallback(List<Question> questions);
+        public static void FindXamarinQuestions(FindXamarinQuestionsCallback done)
+        {
+            RestClient client = new RestClient("https://api.stackexchange.com/2.2");
+            RestRequest request = new RestRequest("/search", Method.GET);
+
+            request.AddParameter("site", "stackoverflow");
+            request.AddParameter("tagged", "xamarin");
+            request.AddParameter("sort", "activity");
+            request.AddParameter("order", "desc");
+
+            request.RootElement = "items";
+
+            client.ExecuteAsync<List<Question>>(request, response =>
+            {
+                if ( response.StatusCode == System.Net.HttpStatusCode.OK )
+                {
+                    done(response.Data);
+                }
+                else
+                {
+                    done(null);
+                }
+            });
+        }
+    }
+}
+
